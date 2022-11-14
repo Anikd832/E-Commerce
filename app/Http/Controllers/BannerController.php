c27ead4946760b726feee79c32e6a0322a32694b
anik das <anikd832@gmail.com>
2022-11-14 16:42:16 +0600

    normal update

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Banner;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BannerController extends Controller
@@ -10,6 +11,11 @@ class BannerController extends Controller
         return view('dashboard.banner.index');
     }
     public function insert(Request $request){
-        
+        $request->validate([
+           'product_category'=>'required',
+        ]);
+        Banner::insert([
+            'created_at'=>Carbon::now(),
+        ]);
     }
 }
